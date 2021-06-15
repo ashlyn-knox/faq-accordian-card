4d4f5cc76c835881c99a4d3823228c28978be7d7
ramyaparimi <rlp.parimi@gmail.com>
2021-06-15 16:52:13 -0500

    some changes

--- faq-accordian/src/App.js
+++ faq-accordian/src/App.js
@@ -2,23 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const faqList = [
+    {
+        que1: "quest",
+        ans1: "ans"
+    },
+    {
+        que2: "quest",
+        ans3: "ans",
+    },
+    {
+        que3: "quest",
+        ans3: "ans",
+    },
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {faqList.que1}
   );
 }
 
